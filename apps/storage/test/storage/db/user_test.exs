d04200cb39e8d5c5c55f49f203eda84074234110
Sergey Gernyak <sergeg1990@gmail.com>
2016-10-21 12:58:19 +0300

    Added tests for models

--- apps/storage/test/storage/db/user_test.exs
+++ apps/storage/test/storage/db/user_test.exs
@@ -1,5 +1,4 @@
 defmodule Storage.DB.UserTest do
-  # use ExUnit.Case, async: true
   use TestCaseWithDbSandbox
 
   alias Storage.DB.User
@@ -14,12 +13,22 @@ defmodule Storage.DB.UserTest do
     refute User.signed_in?(user)
   end
 
-  test "fetch_by_login when nil is passed it returns nil" do
-    assert is_nil User.fetch_by_login(nil)
-  end
-
   test "is_in_training? returns true if user has at least one in progress training word" do
     user = create(:user) |> with_word_training(status: "in_progress")
     assert User.is_in_training?(user)
+  end
+
+  test "is_in_training? returns false if user has no in progress training word" do
+    user = create(:user)
+    refute User.is_in_training?(user)
+  end
+
+  test "fetch_by_login returns nil if nil is passed as login" do
+    assert is_nil(User.fetch_by_login(nil))
+  end
+
+  test "fetch_by_login returns user if such one exists with passed login" do
+    user = create(:user)
+    assert %User{} = User.fetch_by_login(user.login)
   end
 end
